9d6979e4081385747ab80905820557e431a77320
seema <seema@DESKTOP-9RUA99R>
2020-09-07 14:33:31 -0700

    Started level 2

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,5 +1,11 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
@@ -7,12 +13,31 @@ public class _01_RobotRace {
 		Robot robots[] = new Robot[5];
 	//3. use a for loop to initialize the robots.
 		for (int i = 0; i < robots.length ; i++) {
-		robots[i] = new Robot();	
+		robots[i] = new Robot();
+		robots[i].setX(500);
+		robots[i].setY(500);
+		
 		}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 		
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+		int winner = -1;
+		boolean win = false;
+while(!win) {
+		Random ran = new Random();
+for (int i = 0; i < robots.length; i++) {
+	robots[i].move(ran.nextInt(50));
+	if (robots[i].getY()<0) {
+		win = true;
+		winner = i;
+		break;
+	}
+}
+}
+
+		JOptionPane.showMessageDialog(null, "The winner is: " + winner);
+	}
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
